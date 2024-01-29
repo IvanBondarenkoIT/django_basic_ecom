83d2b7a87c0174a29e408bb11f26586379d8eaf6
Ivan Bondarenko <krenkroyt@gmail.com>
2024-01-29 11:49:47 +0400

    added: media fetch data from DB

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from employees.models import Employee
+
 
 def home(request):
-    return render(request, "home.html")
+    _employees = Employee.objects.all()
+    content = {"employees": _employees}
+    return render(request, "home.html", context=content)
